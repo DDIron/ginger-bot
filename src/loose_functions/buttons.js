0e826f54879bccf1e48ab7a99e443c41f49186f0
DDIron <sourlemonmachine@gmail.com>
2022-10-28 16:44:01 +1300

    stop button fixed

--- src/loose_functions/buttons.js
+++ src/loose_functions/buttons.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const { getVoiceConnection } = require("@discordjs/voice");
-const { channel } = require("diagnostics_channel");
 
 module.exports = {
 	execute(interaction) {
@@ -41,10 +38,6 @@ module.exports = {
             channel.send("✅ Skipping the current track...");
         } else if (interaction.customId == "buttonStop") {
             // STOP
-            const fileData = fs.readFileSync("./src/gingerDatabase.json");
-		    const musicQueues = JSON.parse(fileData);
-            musicQueues[interaction.guildId] = [];
-            fs.writeFile("./src/gingerDatabase.json", JSON.stringify(musicQueues, null, 2), function finished(e) {});
             guildQueue.stop();
             guildQueue.clearQueue();
             channel.send("✅ Music has been stopped.");
